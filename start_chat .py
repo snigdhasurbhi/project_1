8e19bd69a2eed2bde25f3c958f6b6faecceefc39
snigdhasurbhi <snigdhasurbhi12@gmail.com>
2017-08-21 21:29:05 +0530

    if else

--- start_chat .py
+++ start_chat .py
@@ -1,3 +1,15 @@
-#start_chat function defination.
-def start_chat (name, age, rating, status) :
-    from globals import
+# start_chat() function definition.
+def start_chat(name, age, rating):
+    show_menu = True
+    while show_menu:
+        menu_choices = "What do you want to dp ? \n 1. Add Status. \n 2. Close Application"
+        result = int(raw_input(menu_choices))
+
+        # validating users input
+        if (result == 1):
+            # action 1
+            pass
+        elif (result == 2):
+            show_menu = False
+        else:
+            print "wrong choice try again."
