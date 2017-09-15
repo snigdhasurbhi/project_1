6b1eda6d30810f68c3aed72c6c5ca01a06bfde14
snigdhasurbhi <snigdhasurbhi12@gmail.com>
2017-09-15 18:49:59 +0530

    global issue

--- start_chat.py
+++ start_chat.py
@@ -1,14 +1,43 @@
+#IMPORTING TWO FILES HERE
+from add_status import add_status
+from add_friend import add_friend
+
 #start_chat() function definition
-def start_chat(name, age, rating, s):
-    show_menu = True
-    while show_menu:
-        menu_choices = "What do you want to do ? \n 1. ADD STATUS \n 2. CLOSE APPLICATION"
+def start_chat(name, age, rating, status):
+    from  global import current_status_message
+    # validating user's details
+    error_message = None  # variable for storing error message
+    if not (age > 12 and age < 50):
+        error_message = " INVALID AGE. PROVIDE VALID AGE"
+        print error_message
+    else:
+        welcome_message = "AUTHENTICATION COMPLETED. Welcome\n  " \
+                          "Name : " + name + "\n" \
+                          "Age : " + str(age) + "\ n" \
+                          "\Rating: " + str(rating) + "\n " \
+                          "Proud to have you here"
+        print welcome_message
+
+        # displaying menu for user
+        show_menu = True
+        while show_menu:
+            menu_choices = "What do you want to do ? \n" \
+                       "1. ADD STATUS \n " \
+                       " 2.ADD A FRIEND \n" \
+                       " 3.SEND A SECRET MESSAGE \n" \
+                       " 4.READ A SECRET MESSAGE \n" \
+                       " 5.READ CHATS FROM A USER \n" \
+                       " 6. CLOSE APPLICATION \n"
         result = int(raw_input(menu_choices))
 
         #validating user input
         if result == 1:
             current_status_message = add_status(current_status_message)
         elif (result == 2):
+            number_of_friends = add_friend()
+            print "You have %d friends" % (number_of_friends)
+        elif (result == 6):
+            # close application
             show_menu = False
         else:
              print "wrong choice.Try again later."
